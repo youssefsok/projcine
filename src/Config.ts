a6df331acf471b8acdf4db4eea5e13097af8dc7b
slamaali@outlook.fr <slamaali@outlook.fr>
2020-01-22 15:45:15 +0100

    fix

--- src/Config.ts
+++ src/Config.ts
@@ -2,6 +2,6 @@ export class Config {
 
   public static apiUrl = ' https://funflix.herokuapp.com';
   public static stripePublishKey = "pk_test_5a1etS5nLFZIvKw0thzIKe8K00SR3neeGB"
-  public static host ="http://localhost:4200"
+  public static host ="https://yousseff97.github.io/projcine"
 
 }
